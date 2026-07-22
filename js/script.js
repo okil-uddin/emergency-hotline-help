86fff50214fdc2498e0ad09c810035d5ba49c90a
Okil Uddin <okiluddin1999@gmail.com>
2026-07-23 00:01:23 +0600

    call function

--- js/script.js
+++ js/script.js
@@ -19,3 +19,33 @@ for (const heart of hearts) {
         newHeartNum.innerText = heartNum + 1;
     })
 }
+
+// Call function
+
+const calls = document.getElementsByClassName('call-btn');
+
+const coinDisplay = getInnerText('coin-num');
+
+const historyPart = document.getElementById('call-history');
+
+const clearBtn = document.getElementById('clear-btn');
+
+for (const call of calls) {
+    call.addEventListener('click', function () {
+
+        const currentCoin = coinDisplay;
+
+        if (currentCoin < 20) {
+            alert('Not enough coins!');
+            return;
+        }
+
+        const serviceName = this.getAttribute('data-name');
+        const serviceNum = this.getAttribute('data-num');
+
+        coinDisplay.innerText = currentCoin - 20;
+
+        alert('Calling ' + serviceName + ': ' + serviceNum);
+
+    })
+}
