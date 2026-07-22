1dca997640ee0ff9649b054beabf8144d0cafcdf
Okil Uddin <okiluddin1999@gmail.com>
2026-07-22 19:17:03 +0600

    heart icon function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-console.log('hello');
+// Sheard Functions
+function getInnerText(id) {
+    const input = document.getElementById(id);
+    const inputValue = input.innerText;
+    const innerTextValue = parseInt(inputValue);
+    return innerTextValue;
+}
+
+// Heart Function
+const hearts = document.getElementsByClassName('hrt-btn');
+
+for (const heart of hearts) {
+    heart.addEventListener('click', function () {
+
+        const heartNum = getInnerText('heart-txt');
+
+        const newHeartNum = document.getElementById('heart-txt');
+
+        newHeartNum.innerText = heartNum + 1;
+    })
+}
